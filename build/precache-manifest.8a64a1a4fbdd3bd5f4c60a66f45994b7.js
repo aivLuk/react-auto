a3fb5f67701196fac7e336c590c3a1808f076183
aivLuk <57671979+aivLuk@users.noreply.github.com>
2020-02-04 12:14:27 +0200

    Add files via upload

---
+++ build/precache-manifest.8a64a1a4fbdd3bd5f4c60a66f45994b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f418714a1cbf09b830d684a80b2385c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62582d9170bb2e14de1f",
+    "url": "/static/css/main.ef17e681.chunk.css"
+  },
+  {
+    "revision": "c97cbe7c6078712ef017",
+    "url": "/static/js/2.9eb2b4a9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9eb2b4a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62582d9170bb2e14de1f",
+    "url": "/static/js/main.14640dc4.chunk.js"
+  },
+  {
+    "revision": "83d91cdd5c395e94cb06",
+    "url": "/static/js/runtime-main.1bbfc418.js"
+  },
+  {
+    "revision": "3d7f6366f09f7334694c86f3d458a88a",
+    "url": "/static/media/swed.3d7f6366.png"
+  }
+]);
